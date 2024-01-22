94171ab95a469472060953669d5894f6a57f8752
sotonyejoe <sotonye28@gmail.com>
2024-01-22 09:51:51 -0800

    worked on the javascript, sidebar,themetoggle, product

---
+++ order.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Rynce Lambert',
+        productNumber: '3453',
+        productpayment: 'due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Game Pad',
+        productNumber: '3455',
+        productpayment: 'paid',
+        shipping: 'delivered'
+    },
+    {
+        productName: 'Mouse',
+        productNumber: '3444',
+        productpayment: 'paid',
+        shipping: 'delivered'
+    },
+    {
+        productName: 'Rynce Lambert',
+        productNumber: '2334',
+        productpayment: 'paid',
+        shipping: 'delivered'
+    },
+    {
+        productName: 'game wheel',
+        productNumber: '3453',
+        productpayment: 'due',
+        shipping: 'pending'
+    },
+]
